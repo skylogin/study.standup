de08ae47af264beddbae925c1b8eb83a264121dc
Chanho Jung <skylogin@gmail.com>
2018-09-26 13:57:20 +0900

    add CardList. modify Card. using Embed.ly, but some issue(CORS, API key..)

---
+++ src/EmbedlyDao.js
@@ -0,0 +1,20 @@
+import request from "axios";
+
+export default function getEmbedly(url) {
+  // return request.getComputedStyle("https://api.embedlyKey.com/1/oembed", {
+  // embed.ly ㅅㅓ비스가 유료화되어서 유튜브로 대체
+  return request.get("https://www.youtube.com/oembed", {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "json"
+    },
+    crossdomain: true,
+    params: {
+      url: url,
+      //key: embedlyKey,
+      format: "json"
+    }
+  });
+}
+
+//https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v=i_0CMuWnAJs&format=json
---
+++ src/components/CardList.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import Card from "./Card";
+import "./CardList.css";
+
+class CardList extends Component {
+  createCard(item, index) {
+    return (
+      <li className="list_row" key={item.key}>
+        <pre className="common_margin grey_text">{item.content}</pre>
+        {item.cardInfo ? <Card cardInfo={item.cardInfo} /> : ""}
+      </li>
+    );
+  }
+
+  render() {
+    return <ul>{this.props.articles.map(this.createCard)}</ul>;
+  }
+}
+
+export default CardList;
